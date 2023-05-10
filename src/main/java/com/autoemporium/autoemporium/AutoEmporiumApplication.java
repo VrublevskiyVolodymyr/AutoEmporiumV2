d7fa5cc7c5360a16c65a6587e5e78d4908d51a01
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-05-10 17:39:44 +0300

    Initial commit

---
+++ src/main/java/com/autoemporium/autoemporium/AutoEmporiumApplication.java
@@ -0,0 +1,13 @@
+package com.autoemporium.autoemporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoEmporiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoEmporiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autoemporium/autoemporium/AutoEmporiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoemporium.autoemporium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEmporiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
